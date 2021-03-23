b137135ee3e5abcc31d799413a5384dac3f4946f
Josh Albrecht <thejash@gmail.com>
2021-03-23 16:14:20 -0700

    Bump version. Fix locked dependencies to be more flexible

--- jupyter_ascending/_version.py
+++ jupyter_ascending/_version.py
@@ -5,5 +5,5 @@
 # Distributed under the terms of the Modified BSD License.
 
 # NOTE: Must keep in sync w/ pyproject.toml to publish
-version_info = (0, 1, 18)
+version_info = (0, 1, 19)
 __version__ = ".".join(map(str, version_info))
